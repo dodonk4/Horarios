10ec56bb53352ff719531e1a1ac80180662f865b
Ismael Agustín <50113883+dodonk4@users.noreply.github.com>
2023-05-05 13:01:40 -0300

    Update db.js

--- db.js
+++ db.js
@@ -19,12 +19,16 @@ const { Pool } = require('pg');
 //   port: 5432
 // });
 
+// const pool = new Pool({
+//   host: process.env.POSTGRES_HOST,
+//   user: process.env.POSTGRES_USER,
+//   password: process.env.POSTGRES_PASSWORD,
+//   database: process.env.POSTGRES_DATABASE,
+//   port: 5432
+// });
+
 const pool = new Pool({
-  host: process.env.POSTGRES_HOST,
-  user: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DATABASE,
-  port: 5432
-});
+  connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+})
 
 module.exports = pool;
